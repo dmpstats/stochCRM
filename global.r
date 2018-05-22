99c06e1b0610e17692183a80998bbd307803e851
Bruno <bruno@dmpstats.com>
2018-05-22 15:54:12 +0100

    BC: automatic installation of missing packages

--- global.r
+++ global.r
@@ -4,27 +4,29 @@
 #'  - v2.1.1: Updated with the latest version of the sCRM model function - Carl made it faster to run and cleaned the R code 
 #' 
 
-library(shiny)
-library(shinydashboard)
-library(rhandsontable)
-library(plyr)
-library(tidyverse)
-library(magrittr)
-library(stringr)
-library(shinyBS)
-library(msm)
-library(shinyjs)
-library(shinyWidgets)
-library(data.table)
-library(DT)
-library(zip)
-library(RColorBrewer)
-library(pracma)
-library(d3heatmap)
+# function in package "pacman" to load packages, automatically installing the ones missing
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(shiny, 
+               shinydashboard, 
+               rhandsontable, 
+               plyr, 
+               tidyverse, 
+               magrittr, 
+               stringr,
+               shinyBS, 
+               msm, 
+               shinyjs, 
+               shinyWidgets, 
+               data.table, 
+               DT, 
+               zip, 
+               RColorBrewer, 
+               pracma, 
+               d3heatmap)
 
-source("helpers.r")
+
 source("BandModel_function.R")
-
+source("helpers.r")
 
 species <- sort(c("Arctic Skua", "Northern Fulmar", "Great Black backed Gull", "Common Guillemot", "Northern Gannet",
              "Black legged Kittiwake", "Lesser Black Backed Gull", "Little Auk", "Atlantic Puffin", 
